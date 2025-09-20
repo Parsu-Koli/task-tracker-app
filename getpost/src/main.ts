0ba14d14912c1796ee88530342b7c06777723495
Parsu-Koli <koliprashant11112@gmail.com>
2025-09-20 19:14:15 +0530

    Delete getpost directory

--- getpost/src/main.ts
+++
@@ -1,8 +0,0 @@
-// main.ts
-import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app/app.component';
-import { provideHttpClient } from '@angular/common/http';
-
-bootstrapApplication(AppComponent, {
-  providers: [provideHttpClient()]
-});
